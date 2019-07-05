db5b48cf83e84c048775f53b3f603be65078d3be
letuzj <letuzj@qq.com>
2019-07-05 17:14:40 +0800

    新添加组合模式

---
+++ src/org/ting/pattern/composite/Employee.java
@@ -0,0 +1,46 @@
+package org.ting.pattern.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 员工类
+ *
+ * @author 张韧炼
+ * @create 2019-07-05 下午4:42
+ **/
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee employee) {
+        subordinates.add(employee);
+    }
+
+    public void remove(Employee employee) {
+        subordinates.remove(employee);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", dept='" + dept + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
